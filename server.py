510f58c62bc61d4aa2d66f58424e733b600dce81
Stephen LaPorte <stephen.laporte@gmail.com>
2013-09-30 02:13:26 +0000

    random seed api, jsonp response will fall back to json response if you dont give it a callback

--- server.py
+++ server.py
@@ -6,24 +6,29 @@ import codecs
 import os
 import random
 
-
-def random_oggs(request, k=5):
-    callback = request.values.get('callback')
-    oggs = codecs.open('commons_oggs_Sept_2013.txt', 'r', encoding='utf-8')
-    ogg_lines = oggs.readlines()
-    ret = [o.strip() for o in random.sample(ogg_lines, int(k))]
-    oggs.close()
-    return ret, callback
-
+DEFAULT_K = 5
 
 class JSONPRender(JSONRender):
     def __call__(self, context):
         data, jsonp_cb = context
         json_iter = self.json_encoder.iterencode(data)
-        resp = Response(''.join([jsonp_cb, '(', ''.join([r for r in json_iter]), ')']), 
-                        mimetype="application/javascript")
+        if jsonp_cb:
+            resp = Response(''.join([jsonp_cb, '(', ''.join([r for r in json_iter]), ')']), 
+                            mimetype="application/javascript")
+            
+        else:
+            resp = Response(json_iter, mimetype="application/json")
         resp.mimetype_params['charset'] = self.json_encoder.encoding
         return resp
+
+def random_oggs(request, seed=None, k=DEFAULT_K):
+    callback = request.values.get('callback')
+    oggs = codecs.open('commons_oggs_Sept_2013.txt', 'r', encoding='utf-8')
+    ogg_lines = oggs.readlines()
+    random.seed(seed)
+    ret = [o.strip() for o in random.sample(ogg_lines, int(k))]
+    oggs.close()
+    return ret, callback
 
 if __name__ == '__main__':
     render_jsonp = JSONPRender()
@@ -31,7 +36,9 @@ if __name__ == '__main__':
     print cur_dir
 
     routes = [('/rand', random_oggs, render_jsonp),
-              ('/rand/<k>', random_oggs, render_jsonp)]
+              ('/rand/<k>', random_oggs, render_jsonp),
+              ('/rands/<seed>', random_oggs, render_jsonp),
+              ('/rands/<seed>/<k>', random_oggs, render_jsonp)]
 
     app = Application(routes)
     app.serve(static_path=cur_dir)
